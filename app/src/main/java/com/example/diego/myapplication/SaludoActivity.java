0bafe6a18be6d28c9e156d073c34a192a1c61c5e
diegocangi <diegocangi@gmail.com>
2018-05-05 18:41:54 -0300

    Splash(sin imagen) y ListView(solo mostrarlo)

--- app/src/main/java/com/example/diego/myapplication/SaludoActivity.java
+++ app/src/main/java/com/example/diego/myapplication/SaludoActivity.java
@@ -2,16 +2,28 @@ package com.example.diego.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class SaludoActivity extends AppCompatActivity {
 
     private TextView txtSaludo;
 
+    ListView listaPlanetas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+
+
+
+        listaPlanetas = (ListView)findViewById(R.id.lv01);
+
+        ArrayAdapter <CharSequence> adaptador = ArrayAdapter.createFromResource( this, R.array.listado, android.R.layout.simple_list_item_1);
+
+        listaPlanetas.setAdapter(adaptador);
 
         //Localizar los controles
         txtSaludo = (TextView)findViewById(R.id.txtSaludo);
---
+++ app/src/main/java/com/example/diego/myapplication/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.diego.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {         //extends Activity {
+
+    // Duración en milisegundos que se mostrará el splash
+    private final int DURACION_SPLASH = 3000; // 3 segundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       // Tenemos una plantilla llamada splash.xml donde mostraremos la información que queramos (logotipo, etc.)
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(com.example.diego.myapplication.SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
