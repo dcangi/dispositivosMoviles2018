9bfa88c81c02e570817c4fb9c0dc0820097b99ec
diegocangi <diegocangi@gmail.com>
2018-05-05 19:25:33 -0300

    Agrega SharedPreferences

--- app/src/main/java/com/example/diego/myapplication/MainActivity.java
+++ app/src/main/java/com/example/diego/myapplication/MainActivity.java
@@ -1,27 +1,58 @@
 package com.example.diego.myapplication;
 //..
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 //..
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtNombre;
     private Button btnAceptar;
+    Button btnGuardar;
+    Button btnMostrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Context contexto = this;
+        SharedPreferences sharprefs = getSharedPreferences("ArchivoSP", MODE_PRIVATE);
+
         //Obtenemos una referencia a los controles de la interfaz
         txtNombre = (EditText)findViewById(R.id.txtNombre);
         btnAceptar = (Button)findViewById(R.id.btnAceptar);
+        btnGuardar = (Button)findViewById(R.id.btnGuardar);
+        btnMostrar = (Button)findViewById(R.id.btnMostrar);
+
+        btnGuardar.setOnClickListener( new View.OnClickListener(){
+            public void onClick( View v ){
+
+                SharedPreferences sharpref = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharpref.edit();
+                editor.putString("MiDato", txtNombre.getText().toString());
+
+                editor.commit();    // guardo como preferencia compartida...
+            }
+        });
+
+        btnMostrar.setOnClickListener( new View.OnClickListener(){
+            public void onClick( View v ){
+
+                SharedPreferences sharpref = getPreferences(MODE_PRIVATE);
+                String valor = sharpref.getString("MiDato", "No Hay dato");
+                Toast.makeText(getApplicationContext(), "Dato Guardado: "+valor, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         //Implementamos el evento click del botón
         btnAceptar.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
                 //Iniciamos la nueva actividad
                 startActivity(intent);
             }
+
+
+
+
+
         });
     }
 }
